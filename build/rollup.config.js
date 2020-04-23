073864fb9376c2803e265fde648f4bbcd58d142f
フィリップ <phil+git@fillip.pro>
2020-04-23 10:42:15 +0300

    Removed CommonJS dependency.

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,12 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
 const output = [
-    { format: 'cjs', suffix: '.cjs' },
-    { format: 'amd', suffix: '.amd' },
     { format: 'es', suffix: '' },
-    { format: 'umd', suffix: '.umd' }
 ].map((config) => {
     return {
         dir: 'lib',
@@ -24,17 +20,9 @@ const plugins = [
     resolve({
         browser: true,
         mainFields: [
-            'jsnext:main',
             'main',
             'module'
         ]
-    }),
-    commonjs({
-        exclude: 'node_modules/process-es6/**',
-        include: [
-        ],
-        namedExports: {
-        }
     }),
     typescript({
         tsconfig: 'src/tsconfig.json'
